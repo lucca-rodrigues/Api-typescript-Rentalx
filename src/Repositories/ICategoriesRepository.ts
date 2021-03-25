d5203f109ea866fca25f67ec116fdd560629accb
Lucca Rodrigues <contato.luccarodrigues@gmail.com>
2021-03-25 13:14:28 -0300

    solid - substitution principe

---
+++ src/Repositories/ICategoriesRepository.ts
@@ -0,0 +1,14 @@
+import { Category } from '../Models/Category';
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+interface ICategoriesRepository {
+  findByName(name: string): Category;
+  show(): Category[];
+  create({name, description}: ICreateCategoryDTO): void;
+}
+
+export { ICategoriesRepository, ICreateCategoryDTO };
