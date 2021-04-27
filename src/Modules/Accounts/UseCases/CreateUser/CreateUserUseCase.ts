eba4840ee82fc7916a2e0977c6e46e804a6d9394
Lucca Rodrigues <contato.luccarodrigues@gmail.com>
2021-04-27 19:12:16 -0300

    add error

--- src/Modules/Accounts/UseCases/CreateUser/CreateUserUseCase.ts
+++ src/Modules/Accounts/UseCases/CreateUser/CreateUserUseCase.ts
@@ -22,7 +22,7 @@ class CreateUserUseCase {
     const userAlreadyExists = await this.usersRepository.findByEmail(email);
 
     if (userAlreadyExists) {
-      throw new AppError('User already exists');
+      throw new Error('User already exists');
     }
 
     const passwordHash = await hash(password, 8);
