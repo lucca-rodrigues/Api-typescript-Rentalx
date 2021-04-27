80f0fd0b905d26586be93e2327ed41da79989d54
Lucca Rodrigues <contato.luccarodrigues@gmail.com>
2021-04-27 20:05:20 -0300

    add exceptions

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,34 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
-import { router } from './Routes';
-
+import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
+
+import { AppError } from './Errors/AppError';
+import { router } from './Routes';
 import swaggerFile from './swagger.json';
 
-import "./Database";
-import "./Shared/Container"
+import './Database';
+import './Shared/Container';
 
 const app = express();
 
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use(router)
 
-app.listen(3333, () => console.log("App started on port 3333"));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+
+  console.log(err);
+
+  return res.status(500).json({
+    status: 'error',
+    message: `Internal server error - ${err.message}`,
+  });
+});
+
+app.listen(3333, () => console.log('Server is Running! 🎉'));
