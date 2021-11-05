01d44a7af35209194f2665b9729424f805a9662d
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2021-11-05 19:12:01 -0300

    create specification  repository

--- src/Modules/Cars/Repositories/SpecficationRepository.ts
+++ src/Modules/Cars/Repositories/SpecficationRepository.ts
@@ -1,11 +1,26 @@
+import { SpecificationModel } from "../Models/SpecificationModel";
 import {
   ISpecificationRepository,
   ICreateSpecificationDTO,
 } from "./ISpecificationRepository";
 
 class SpecificationRepository implements ISpecificationRepository {
+  private specifications: SpecificationModel[];
+
+  constructor() {
+    this.specifications = [];
+  }
+
   create({ name, description }: ICreateSpecificationDTO): void {
-    throw new Error("Method not implemented.");
+    const specification = new SpecificationModel();
+
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.specifications.push(specification);
   }
 }
 
