a4c7ab57bed9c0f091f4537a13fb4c7da6c0a052
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2021-11-09 17:09:00 -0300

    add swagger configs and route documentation

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+
 import { router } from "./Routes";
+
+import swaggerFile from "./swagger.json";
 
 const app = express();
 app.use(express.json());
 
 app.get("/", (request, response) => {
-  response.send("Hello World!");
+  response.send("Api Ok!");
 });
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 app.listen(3333, () => console.log("Server started on port 3333"));
