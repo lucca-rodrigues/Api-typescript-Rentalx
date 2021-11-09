b3d694b62c1be547db29391b95fd95ed565d3dd2
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2021-11-09 16:40:17 -0300

    add upload file csv

--- src/Modules/Cars/UseCases/UploadFilesCategory/UploadFilesCategoryUseCase.ts
+++ src/Modules/Cars/UseCases/UploadFilesCategory/UploadFilesCategoryUseCase.ts
@@ -1,6 +1,16 @@
+import csvParse from "csv-parse";
+import fs from "fs";
 class UploadFilesCategoryUseCase {
   execute(file: Express.Multer.File): void {
-    console.log(file);
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 
